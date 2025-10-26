a54b92e3af459dff86c6dfaec163722ff3a7ce89
huseyinbenli <huseyinbenli@outlook.fr>
2025-10-26 20:04:18 +0100

    homepage is updated with a simple style

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import "./style.css";
+import hompageLoader from "./homepage.js";
+
+const content = document.querySelector("#content");
+
+const homepage = hompageLoader();
+
+content.appendChild(homepage);
--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,6 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
-
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
