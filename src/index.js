614152c548aed1eefa2e96b2c73b46103448cd34
huseyinbenli <huseyinbenli@outlook.fr>
2025-10-26 22:01:58 +0100

    menupage is updated with basic style and images

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import "./style.css";
 import hompageLoader from "./homepage.js";
+import menuLoader from "./menupage.js";
 
+// elements
 const content = document.querySelector("#content");
+const homeBtn = document.querySelector(".homeBtn");
+const menuBtn = document.querySelector(".menuBtn");
+const aboutBtn = document.querySelector(".aboutBtn");
 
 const homepage = hompageLoader();
+const menupage = menuLoader();
 
 content.appendChild(homepage);
+
+homeBtn.addEventListener("click", () => {
+  content.innerHTML = "";
+  content.appendChild(homepage);
+});
+
+menuBtn.addEventListener("click", () => {
+  content.innerHTML = "";
+  content.appendChild(menupage);
+});
---
+++ src/menupage.js
@@ -0,0 +1,73 @@
+// images
+import buffaloImg from "./assets/buffalo.webp";
+import burgerImg from "./assets/burger.webp";
+import chickenImg from "./assets/chicken.webp";
+import jalapenoImg from "./assets/jalapeno.webp";
+import soupImg from "./assets/soup.webp";
+import lasagnaImg from "./assets/lasagna.webp";
+
+export default () => {
+  const menuList = [
+    {
+      title: "Buffalo Chicken Baked Ziti",
+      desc: "Creamy and cheesy Buffalo-flavored chicken baked ziti!",
+      img: buffaloImg,
+    },
+    {
+      title: "Cream Cheese-Jalapeño Hamburgers",
+      desc: "Burgers, made with cream cheese and jalapeno between two patties ",
+      img: burgerImg,
+    },
+    {
+      title: "Million Dollar Chicken Breasts",
+      desc: "Chicken breasts are topped with cream cheese, sour cream, cheddar, and bacon",
+      img: chickenImg,
+    },
+    {
+      title: "Jalapeño Cream Cheese Chicken Enchiladas",
+      desc: "A heavenly dish with jalapeño cream cheese and shredded chicken",
+      img: jalapenoImg,
+    },
+    {
+      title: "Potato Soup with Cream Cheese",
+      desc: " This a well balanced, full-flavored soup, with a hint of trailing heat compliments of cayenne.",
+      img: soupImg,
+    },
+    {
+      title: "Creamy Chicken Lasagna",
+      desc: "This chicken lasagna is made with tender chicken breasts, red sauce, and lots of mozzarella cheese for a fantastic flavor combination",
+      img: lasagnaImg,
+    },
+  ];
+  // elements
+  const wrapperDiv = document.createElement("div");
+  menuList.forEach((item) => {
+    // creating elements for each item in the array
+    const card = document.createElement("div");
+    const imgWrapper = document.createElement("div");
+    const cardImg = document.createElement("img");
+    const cardTitle = document.createElement("h2");
+    const cardDesc = document.createElement("p");
+
+    // classes
+    wrapperDiv.classList.add("wrapper-div");
+    card.classList.add("card");
+    imgWrapper.classList.add("img-wrapper");
+    cardTitle.classList.add("card-title");
+    cardDesc.classList.add("card-p");
+
+    // content
+    cardImg.src = item.img;
+    cardTitle.innerText = item.title;
+    cardDesc.innerText = item.desc;
+
+    //append
+    imgWrapper.appendChild(cardImg);
+    card.appendChild(imgWrapper);
+    card.appendChild(cardTitle);
+    card.appendChild(cardDesc);
+    wrapperDiv.appendChild(card);
+  });
+
+  return wrapperDiv;
+};
